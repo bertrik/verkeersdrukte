a0a7ba1c9c1d7ab1d6569fee159598114d8a5bfd
Bertrik Sikken <bertrik@sikken.nl>
2024-04-01 01:05:23 +0200

    Update OpenAPIDefinition description
    Remove unneeded dependency, fix tags

--- verkeersdrukte/src/main/java/nl/bertriksikken/verkeersdrukte/app/IVerkeersDrukteResource.java
+++ verkeersdrukte/src/main/java/nl/bertriksikken/verkeersdrukte/app/IVerkeersDrukteResource.java
@@ -15,33 +15,29 @@ import nl.bertriksikken.geojson.FeatureCollection;
 import java.util.Optional;
 
 @OpenAPIDefinition(
-    info = @Info(
-        title = "Verkeersdrukte",
-        description = "Provides near real-time speed/intensity traffic data for motorways in the Netherlands,"
-                    + " based on open data from Nationaal Dataportaal Wegverkeer ( https://opendata.ndw.nu/ )",
-        contact = @Contact(name = "Bertrik Sikken", email = "bertrik@gmail.com")),
+        info = @Info(
+                title = "Verkeersdrukte",
+                description = "Provides near real-time speed/intensity traffic data for motorways in the Netherlands,"
+                        + " based on open data from Nationaal Dataportaal Wegverkeer ( https://opendata.ndw.nu/ )",
+                contact = @Contact(name = "Bertrik Sikken", email = "bertrik@gmail.com")),
         servers = {@Server(url = "https://stofradar.nl"), @Server(url = "http://stofradar.nl:9002")},
-    tags = {
-        @Tag(name = "static", description = "location data"),
-        @Tag(name = "dynamic", description = "traffic data, intensity (vehicles/hour) and speed (km/hour)")
-    })
+        tags = {
+                @Tag(name = "static", description = "location data"),
+                @Tag(name = "dynamic", description = "traffic data, intensity (vehicles/hour) and speed (km/hour)")
+        })
 public interface IVerkeersDrukteResource {
     @Operation(hidden = true)
     void redirectSwagger();
 
-    @Operation(summary = "Get GeoJSON containing all locations")
-    @Tag(name = "static")
+    @Operation(summary = "Get GeoJSON containing all locations", tags = {"static"})
     FeatureCollection getStatic();
 
-    @Operation(summary = "Get static data for a specific location")
-    @Tag(name = "static")
+    @Operation(summary = "Get static data for a specific location", tags = {"static"})
     Optional<FeatureCollection.Feature> getStatic(@PathParam("location") String location);
 
-    @Operation(summary = "Get dynamic traffic data for a specific location")
-    @Tag(name = "dynamic")
+    @Operation(summary = "Get dynamic traffic data for a specific location", tags = {"dynamic"})
     Optional<VerkeersDrukteResource.MeasurementResult> getDynamic(@PathParam("location") String location);
 
-    @Operation(summary = "Get event stream with dynamic traffic data for a specific location")
-    @Tag(name = "dynamic")
+    @Operation(summary = "Get event stream with dynamic traffic data for a specific location", tags = {"dynamic"})
     void getTrafficEvents(@Context Sse sse, @Context SseEventSink sseEventSink, @PathParam("location") String location);
 }
