dc88610c70a1508d165fc70379823233dd73a569
Bertrik Sikken <bertrik@sikken.nl>
2024-08-06 20:34:11 +0200

    Add HTTP header that explicitly indicates that we can accept gzipped data

--- verkeersdrukte/src/main/java/nl/bertriksikken/verkeersdrukte/ndw/INdwApi.java
+++ verkeersdrukte/src/main/java/nl/bertriksikken/verkeersdrukte/ndw/INdwApi.java
@@ -3,6 +3,7 @@ package nl.bertriksikken.verkeersdrukte.ndw;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 public interface INdwApi {
@@ -13,6 +14,7 @@ public interface INdwApi {
      * Downloads a file from <a href="https://opendata.ndw.nu/">NDW open data portaal</a>
      */
     @GET("/{filename}")
+    @Headers({"Accept-Encoding: gzip"})
     Call<ResponseBody> downloadFile(@Path("filename") String filename);
 
 }
